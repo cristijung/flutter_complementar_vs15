9822f6b150ad96fc35ab3c8f5193a109a299797e
CristinaJung <cristijung@gmail.com>
2025-03-19 09:30:54 -0300

    ajuste

--- lib/components/navigationbar.dart
+++ lib/components/navigationbar.dart
@@ -1,31 +1,46 @@
 import 'package:flutter/material.dart';
 
 class CustomNavigationBar extends StatelessWidget {
-  //const CustomNavigationBar({super.key});
-
   final int selectedIndex;
   final Function(int) onItemTapped;
 
-  //Construtor da classe
   const CustomNavigationBar({
     super.key,
     required this.selectedIndex,
     required this.onItemTapped,
-});
+  });
 
   @override
   Widget build(BuildContext context) {
-    return NavigationBar(
-      onDestinationSelected: onItemTapped,
-      selectedIndex: selectedIndex,
-      destinations: const <Widget>[
-        NavigationDestination(icon: Icon(Icons.home), label: 'Home',),
-        NavigationDestination(icon: Icon(Icons.image), label: 'Imagens',),
-        NavigationDestination(icon: Icon(Icons.task), label: 'Tarefas',),
-      ],
-      backgroundColor: Colors.purple,
-      indicatorColor: Colors.pink.shade200,
-      labelBehavior: NavigationDestinationLabelBehavior.alwaysShow,
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [Colors.purple, Colors.pink],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: NavigationBar(
+        onDestinationSelected: onItemTapped,
+        selectedIndex: selectedIndex,
+        destinations: const <Widget>[
+          NavigationDestination(
+            icon: Icon(Icons.home, color: Colors.white),
+            label: '',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.image, color: Colors.white),
+            label: '',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.task, color: Colors.white),
+            label: '',
+          ),
+        ],
+        backgroundColor: Colors.transparent,
+        indicatorColor: Colors.pink.shade200,
+        labelBehavior: NavigationDestinationLabelBehavior.alwaysHide, // ocultando os labels
+      ),
     );
   }
 }
